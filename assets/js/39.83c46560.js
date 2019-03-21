8461fb802f49ebcea7af6b34551fda7a077a27a5
Abdalla Arbab <the94air@gmail.com>
2019-03-21 14:52:32 +0200

    Fix 404 font

--- assets/js/39.83c46560.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{93:function(n,w,o){}}]);
---
+++ assets/js/40.82c4de79.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{93:function(n,w,o){}}]);
